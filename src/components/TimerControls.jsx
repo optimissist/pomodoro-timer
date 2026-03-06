990d1c1efab952957a7bc5cb7fa229c09f840869
Marla <Marla.Schevker@gmail.com>
2026-03-05 18:26:54 -0800

    updated div to <></>

--- src/components/TimerControls.jsx
+++ src/components/TimerControls.jsx
@@ -1,11 +1,11 @@
 function TimerControls({isRunning, onStart, onPause, onReset, onSkip}) {
     return (
-        <div>
+        <>
              <button type="button" onClick={onStart} disabled={isRunning}>Start</button>
              <button type="button" onClick={onPause} disabled={!isRunning}>Pause</button>
              <button type="button" onClick={onReset}>Reset</button>
              <button type="button" onClick={onSkip}>Skip</button>
-        </div>
+        </>
     )
 }
 
